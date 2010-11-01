7faecaf1a4ee5fd3e25e2a1facfbfdebb15b596a
Josh Allmann <joshua.allmann@gmail.com>
2010-10-31 19:25:10 -0700

    Move things around the rxt_node struct and clean up comments. Breaks ABI.

--- radix.h
+++ radix.h
@@ -3,11 +3,11 @@
 
 typedef struct rxt_node {
     int color;
-    char *key; // only stores pos and beyond
+    char *key;
+    void *value;
+    int pos; // bit index of the key to compare at (critical position)
     int level; // tree level; for debug only
     int parent_id; //for debug only
-    int pos;
-    void *value;
     struct rxt_node *parent;
     struct rxt_node *left;
     struct rxt_node *right;
